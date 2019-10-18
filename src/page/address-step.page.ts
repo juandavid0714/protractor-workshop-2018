8c14e6cdfedb078df6707fa7d8a5d36a80b05c43
Juan David <jmunozv@psl.com.co>
2019-10-18 11:07:54 -0500

    Removiendo la pagina product added

--- src/page/address-step.page.ts
+++ src/page/address-step.page.ts
@@ -1,14 +1,23 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, protractor, browser } from 'protractor';
 
 export class AddressStepPage {
   private productImage: ElementFinder;
+  private addToCarButton: ElementFinder;
+  private EC = protractor.ExpectedConditions;
 
   constructor () {
     this.productImage = $('#center_column a.product_img_link > img');
+    this.addToCarButton = $('#add_to_cart > button');
   }
 
   public async selectProduct(): Promise<void> {
+    const isClickable = this.EC.elementToBeClickable(this.productImage);
+    await browser.wait(isClickable, 2000);
     await this.productImage.click();
+  }
+
+  public async addToCar(): Promise<void> {
+    await this.addToCarButton.click();
   }
 
 }
